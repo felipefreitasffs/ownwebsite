fbaf7479dcf21d56e748933da81390348cf91479
Felipe Freitas <felipefreitas.ffs@gmail.com>
2025-06-09 20:32:12 +0000

    ajuste pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Adicione esta linha para corrigir o build
+  output: 'standalone',
+
   typescript: {
     ignoreBuildErrors: true,
   },
