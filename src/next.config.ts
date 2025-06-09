899e907ab324d5898ae5a0d409b5e30a3072371b
Felipe Freitas <felipefreitas.ffs@gmail.com>
2025-06-09 18:56:56 +0000

    o pipeline deu esse erro
    #13 ERROR: process "/bin/sh -c if [ ! -d \".ne

---
+++ src/next.config.ts
@@ -0,0 +1,25 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: false, // Changed from true
+  },
+  eslint: {
+    ignoreDuringBuilds: false, // Changed from true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
