4b80725e0cdd7e155cef4ca5c54cf98db0a6f5ec
Felipe Freitas <felipefreitas.ffs@gmail.com>
2025-06-10 19:33:43 +0000

    o titulo do site deve ser somente meu nome Felipe Freitas e não Felipe F

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/layout/navbar';
 import Footer from '@/components/layout/footer';
 
 export const metadata: Metadata = {
-  title: 'Felipe Freitas Portfolio',
+  title: 'Felipe Freitas',
   description: 'Personal portfolio of Felipe Freitas, Software Engineering Leader.',
 };
 
